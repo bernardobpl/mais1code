45652e93a8133de6ed57c55919db281c86feb436
Bernardo Bruno Pereira Lopes_CONTR <C0620491@vale.com>
2021-12-02 21:49:23 -0300

    descrição do fluxo do git/github

--- desafios/aula4.js
+++ desafios/aula4.js
@@ -1,1 +1,11 @@
 // esse é um arquivo de teste para o github
+
+// git commit -m "alguma mensagem"  =>  salva as alterações feitas pros arquivos que estiverem em "Staged"
+// git push  =>  envia os commits q vc fez no seu computador para o github
+// git pull  =>  vc puxa as atualizações do github caso tenha (ele só puxa as atualizações da sua branch)
+// o que é branch? é um clone do repositório original que vc pode mexer a vontade e depois pode misturar com o repositório original as mudanças que vc fez na sua branch (branch = galho)
+// o que é merge? merge é quando vc tenta passar as atualizações da sua branch para outra (merge = mistura)
+// o que é repositório? são um conjunto de arquivo q vc queira guardar versões dele
+// o que é pull request? quando vc quer fazer um merge, vc precisa de autorização de alguém para aceitar essas mudanças e essa requisição de autorização chama pull request
+// o que é git? é uma ferramenta q cria um repsitório e vai atualizando suas versões para vc
+// o que é o github? é um lugar em que vc pode jogar seus arquivos em um repositório online que pode ser acessado em outros dispositivos ou por outras pessoas e já tem a ferramenta git embutida
