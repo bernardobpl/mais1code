c1221042f7bdebb428f9a79a96bfec9ff9768765
Bernardo Bruno Pereira Lopes_CONTR <C0620491@vale.com>
2021-12-02 21:12:35 -0300

    segundo commit

--- javascript/aula3.js
+++ javascript/aula3.js
@@ -1,17 +1,19 @@
-// const primeiroNome = "Bernardo ";
-// const nomeDoMeio = "Bruno ";
-// const sobrenome = "Pereira Lopes";
+const primeiroNome = "Bernardo ";
+const nomeDoMeio = "Bruno ";
+const sobrenome = "Pereira Lopes";
 
-// function GeraNomeCompleto(nome1, nome2, nome3){
-//   const nomeCompleto = nome1 + nome2 + nome3;
-//   return nomeCompleto;
-// }
+function GeraNomeCompleto(nome1, nome2, nome3){
+  const nomeCompleto = nome1 + nome2 + nome3;
+  return nomeCompleto;
+}
 
-// var teste = GeraNomeCompleto(primeiroNome,nomeDoMeio, sobrenome)
-// console.log(teste);
+var teste = GeraNomeCompleto(primeiroNome,nomeDoMeio, sobrenome)
+console.log(teste);
 
-// console.log( GeraNomeCompleto(primeiroNome,nomeDoMeio, sobrenome) );
+console.log( GeraNomeCompleto(primeiroNome,nomeDoMeio, sobrenome) );
 
+
+//desafio
 // criar uma função pra cada operação básica de uma calculadora
 // soma 1+1 
 // subtracao 1-1
