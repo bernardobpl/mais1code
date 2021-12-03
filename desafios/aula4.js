273bed555f5562136139d7d9040340166f73ab93
Bernardo Bruno Pereira Lopes_CONTR <C0620491@vale.com>
2021-12-02 22:01:04 -0300

    desafio da aula 4

--- desafios/aula4.js
+++ desafios/aula4.js
@@ -1,11 +1,11 @@
-// esse é um arquivo de teste para o github
+// função de 2 grau
+// Ax² + Bx + c = 0
+// vcs vão pegar os valores A, B, C e resolver a equação do segundo grau
+// exemplo https://matematicabasica.net/equacao-do-2-grau-segundo-grau/
+// usar essa equação para testar: x²-5x+6=0
+// dica: a=1 b=-5 c=6
+// resultado 
+// x1 = 3
+// x2 = 2
 
-// git commit -m "alguma mensagem"  =>  salva as alterações feitas pros arquivos que estiverem em "Staged"
-// git push  =>  envia os commits q vc fez no seu computador para o github
-// git pull  =>  vc puxa as atualizações do github caso tenha (ele só puxa as atualizações da sua branch)
-// o que é branch? é um clone do repositório original que vc pode mexer a vontade e depois pode misturar com o repositório original as mudanças que vc fez na sua branch (branch = galho)
-// o que é merge? merge é quando vc tenta passar as atualizações da sua branch para outra (merge = mistura)
-// o que é repositório? são um conjunto de arquivo q vc queira guardar versões dele
-// o que é pull request? quando vc quer fazer um merge, vc precisa de autorização de alguém para aceitar essas mudanças e essa requisição de autorização chama pull request
-// o que é git? é uma ferramenta q cria um repsitório e vai atualizando suas versões para vc
-// o que é o github? é um lugar em que vc pode jogar seus arquivos em um repositório online que pode ser acessado em outros dispositivos ou por outras pessoas e já tem a ferramenta git embutida
+// dar um console.log no x1 e no x2 que for cvalculado pela função de vcs
