706a146959257a5ab246cf21a3b7f7809d5604cf
Bernardo Bruno Pereira Lopes_CONTR <C0620491@vale.com>
2021-12-14 21:05:00 -0300

    resolvendo desafio da aula 5

--- desafios/aula5.js
+++ desafios/aula5.js
@@ -1,7 +1,7 @@
 // imagina que você está indo para um show em uma van com pessoas aleatórias
 // cada pessoa será representada por um objeto contendo nome e idade
 // as pessoas dentro da van serão representadas por uma lista(array) de pessoas
-// ao chegar no show, apenas as pessoas que tem 18 anos ou mais podem entrar
+// ao chegar no show, apenas as pessoas que tem 18 anos ou mais podem entrar 
 // mostre no terminal/console o nome das pessoas que poderão entrar no show
 
 // para se referir à alguma característica(chave) de um objeto usa a seguinte forma exemplo:   pessoa.nome
@@ -23,7 +23,7 @@
 
 const pessoa0 = {
   nome: "Cleiton",
-  idade: 16
+  idade: 16,
 }
 
 const pessoa1 = {
@@ -46,30 +46,93 @@ const pessoa4 = {
   idade: 18
 }
 
+let contador = 0;
+
+// while(contador < 5 ){
+//   if(pessoa0.idade >= 18 ){
+//     console.log(pessoa0.nome)
+//   }
+//   if(pessoa1.idade >= 18 ){
+//     console.log(pessoa1.nome)
+//   }
+//   if(pessoa2.idade >= 18 ){
+//     console.log(pessoa2.nome)
+//   }
+//   if(pessoa3.idade >= 18 ){
+//     console.log(pessoa3.nome)
+//   }
+//   if(pessoa4.idade >= 18 ){
+//     console.log(pessoa4.nome)
+//   }
+
+//   console.log("contador:", contador);
+//   contador ++;
+// }
+
+// for( contador; contador<5; contador++){
+//   if(pessoa0.idade >= 18 ){
+//     console.log(pessoa0.nome)
+//   }
+//   if(pessoa1.idade >= 18 ){
+//     console.log(pessoa1.nome)
+//   }
+//   if(pessoa2.idade >= 18 ){
+//     console.log(pessoa2.nome)
+//   }
+//   if(pessoa3.idade >= 18 ){
+//     console.log(pessoa3.nome)
+//   }
+//   if(pessoa4.idade >= 18 ){
+//     console.log(pessoa4.nome)
+//   }
+
+//   console.log("contador:", contador);
+// }
+
+// function soma(a,b){
+//   return a+b;
+// }
+
+// const resultado = soma(1,2);
+
+
+// const Soma2 = (a,b) => {
+//   return a+b;
+// }
+
+// Soma2(1,2)
+
+
 const pessoas = [ pessoa0, pessoa1, pessoa2, pessoa3, pessoa4 ];
 
 
-
-// pegando o item 0 da lista de pessoas e mostrando o valor
-const itemZeroDaLista = pessoas[0];
-console.log( `Esse é o item zero da lista: ${itemZeroDaLista}` );
-
-
-
-// pegando o valor da propriedade nome da pessoa 2 e mostrando o valor
-const valorDaPropriedadeNomeDoObjetoPessoa2 = pessoa2.nome;
-console.log( `Esse é o valor da propriedade nome da pessoa 2: ${valorDaPropriedadeNomeDoObjetoPessoa2}` );
+pessoas.map( (pessoa) => {
+  if( pessoa.idade >= 18 ){
+    console.log(pessoa.nome)
+  }
+})
 
 
-
-// usando a propriedade idade do objeto pessoa4 para comparar se é maior ou igual a 18, e se for verdade ele irá mostrar o nome da pessoa
-if(pessoa4.idade >= 18){
-  console.log( `A ${pessoa4.nome} pode ir ao show pois tem 18 anos ou mais` )
-}
+// // pegando o item 0 da lista de pessoas e mostrando o valor
+// const itemZeroDaLista = pessoas[0];
+// console.log( `Esse é o item zero da lista: ${itemZeroDaLista}` );
 
 
 
-// a mesma coisa do exemplo anterior, porém usando os colchetes [] para acessar o item e logo depois usar a propriedade idade, caso for verdadeiro irá mostra o nome do item
-if(pessoas[4].idade >= 18){
-  console.log( `A ${pessoas[4].nome} pode ir ao show pois tem 18 anos ou mais` )
-}
+// // pegando o valor da propriedade nome da pessoa 2 e mostrando o valor
+// const valorDaPropriedadeNomeDoObjetoPessoa2 = pessoa2.nome;
+// console.log( `Esse é o valor da propriedade nome da pessoa 2: ${valorDaPropriedadeNomeDoObjetoPessoa2}` );
+
+
+
+// // usando a propriedade idade do objeto pessoa4 para comparar se é maior ou igual a 18, e se for verdade ele irá mostrar o nome da pessoa
+// if(pessoa4.idade >= 18){
+//   console.log( `A ${pessoa4.nome} pode ir ao show pois tem 18 anos ou mais` )
+// }
+
+
+
+// // a mesma coisa do exemplo anterior, porém usando os colchetes [] para acessar o item e logo depois usar a propriedade idade, caso for verdadeiro irá mostra o nome do item
+// if(pessoas[4].idade >= 18){
+//   console.log( `A ${pessoas[4].nome} pode ir ao show pois tem 18 anos ou mais` )
+// }
---
+++ javascript/aula5.js
@@ -0,0 +1,28 @@
+const lista = [ "gabriel", "clara", "mauricio", "jean", "bernardo", "maria" ]
+
+// 0 gabriel
+// 1 clara
+// 2 mauricio
+// 3 jean
+// 4 bernardo
+// 5 maria
+
+// console.log( lista[4] )
+///////////////////////////////////////////////
+const pessoa0 = {
+  idade: 16,
+  nome: "Cleiton",
+}
+
+// console.log(pessoa0.nome)
+
+//////////////////////////////////
+
+const listaDeObjetos2 = [ 
+  {
+    idade: 16, 
+    nome: "Cleiton"
+  }
+]
+
+console.log( listaDeObjetos2[0].nome )
